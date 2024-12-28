b8014c6a21586137c72692236ac1f1e767c81662
Atralupus <me@atralupus.com>
2024-12-29 03:43:40 +0900

    Disable lobby menus

--- src/pages/MainPage/Lobby.tsx
+++ src/pages/MainPage/Lobby.tsx
@@ -5,67 +5,67 @@ import { formatAddress } from "@/utils/format";
 import { NavLink } from "react-router-dom";
 
 interface Navigation {
-  name: string;
-  path: ROUTES;
+	name: string;
+	path: ROUTES;
 }
 
 const NavigationList: Array<Navigation> = [
-  { name: "Market", path: ROUTES.MARKET },
-  { name: "Workshop", path: ROUTES.WORKSHOP },
-  { name: "World boss", path: ROUTES.WORLD_BOSS },
-  { name: "Arena", path: ROUTES.ARENA },
-  { name: "Monster collection", path: ROUTES.MONSTER_COLLECTION },
-  { name: "Patrol reward", path: ROUTES.PATROL_REWARD },
-  { name: "Adventure", path: ROUTES.ADVENTURE },
-  { name: "Collection", path: ROUTES.COLLECTION },
+	// { name: "Market", path: ROUTES.MARKET },
+	// { name: "Workshop", path: ROUTES.WORKSHOP },
+	// { name: "World boss", path: ROUTES.WORLD_BOSS },
+	// { name: "Arena", path: ROUTES.ARENA },
+	// { name: "Monster collection", path: ROUTES.MONSTER_COLLECTION },
+	// { name: "Patrol reward", path: ROUTES.PATROL_REWARD },
+	{ name: "Adventure", path: ROUTES.ADVENTURE },
+	// { name: "Collection", path: ROUTES.COLLECTION },
 ];
 
 const MenuList: Array<Navigation> = [
-  {
-    name: "Ranking",
-    path: ROUTES.RANKING,
-  },
-  {
-    name: "Inventory",
-    path: ROUTES.INVENTORY,
-  },
-  {
-    name: "Production status",
-    path: ROUTES.PRODUCTION_STATUS,
-  },
-  {
-    name: "Mail",
-    path: ROUTES.MAIL,
-  },
-  {
-    name: "Settings",
-    path: ROUTES.SETTINGS,
-  },
+	{
+		name: "Ranking",
+		path: ROUTES.RANKING,
+	},
+	{
+		name: "Inventory",
+		path: ROUTES.INVENTORY,
+	},
+	{
+		name: "Production status",
+		path: ROUTES.PRODUCTION_STATUS,
+	},
+	{
+		name: "Mail",
+		path: ROUTES.MAIL,
+	},
+	{
+		name: "Settings",
+		path: ROUTES.SETTINGS,
+	},
 ];
 
 export default function Lobby() {
-  const { avatarAddress, avatarInfo } = useAuthStore();
+	const { avatarAddress, avatarInfo } = useAuthStore();
 
-  return (
-    <>
-      {avatarInfo && avatarAddress && (
-        <TerminalContainer title="Avatar info" type="sub" className="">
-          <div className="flex items-center gap-3">
-            <p>Name</p>
-            <p>{avatarInfo.name}</p>
-          </div>
-          <div className="flex items-center gap-3">
-            <p>Address</p>
-            <p>{formatAddress(avatarAddress)}</p>
-          </div>
-          <div className="flex items-center gap-3">
-            <p>Level</p>
-            <p>{avatarInfo.level}</p>
-          </div>
-        </TerminalContainer>
-      )}
+	return (
+		<>
+			{avatarInfo && avatarAddress && (
+				<TerminalContainer title="Avatar info" type="sub" className="">
+					<div className="flex items-center gap-3">
+						<p>Name</p>
+						<p>{avatarInfo.name}</p>
+					</div>
+					<div className="flex items-center gap-3">
+						<p>Address</p>
+						<p>{formatAddress(avatarAddress)}</p>
+					</div>
+					<div className="flex items-center gap-3">
+						<p>Level</p>
+						<p>{avatarInfo.level}</p>
+					</div>
+				</TerminalContainer>
+			)}
 
-      <TerminalContainer
+			{/* <TerminalContainer
         title="Menu"
         type="sub"
         className="flex gap-3 flex-wrap"
@@ -77,21 +77,21 @@ export default function Lobby() {
             </NavLink>
           );
         })}
-      </TerminalContainer>
+      </TerminalContainer> */}
 
-      <TerminalContainer
-        title="Main service"
-        type="sub"
-        className="flex gap-3 flex-wrap"
-      >
-        {NavigationList.map(({ name, path }) => {
-          return (
-            <NavLink to={path} key={path}>
-              <button className="btn">{name}</button>
-            </NavLink>
-          );
-        })}
-      </TerminalContainer>
-    </>
-  );
+			<TerminalContainer
+				title="Main service"
+				type="sub"
+				className="flex gap-3 flex-wrap"
+			>
+				{NavigationList.map(({ name, path }) => {
+					return (
+						<NavLink to={path} key={path}>
+							<button className="btn">{name}</button>
+						</NavLink>
+					);
+				})}
+			</TerminalContainer>
+		</>
+	);
 }
